1154e32072885dceec9528b869ceb91cc763c086
Tom Mitchell <tom.0505@hotmail.co.uk>
2017-10-05 02:31:39 +0100

    first commit

---
+++ gol-rgb.cc
@@ -0,0 +1,429 @@
+// -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; -*-
+//
+// This code is public domain
+// (but note, once linked against the led-matrix library, this is
+// covered by the GPL v2)
+//
+// This is a grab-bag of various demos and not very readable.
+#include "led-matrix.h"
+#include "threaded-canvas-manipulator.h"
+#include "transformer.h"
+#include "graphics.h"
+
+#include <assert.h>
+#include <getopt.h>
+#include <limits.h>
+#include <math.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <algorithm>
+
+using std::min;
+using std::max;
+
+#define TERM_ERR  "\033[1;31m"
+#define TERM_NORM "\033[0m"
+
+using namespace rgb_matrix;
+
+volatile bool interrupt_received = false;
+static void InterruptHandler(int signo) {
+  interrupt_received = true;
+}
+
+/*
+ * The following are demo image generators. They all use the utility
+ * class ThreadedCanvasManipulator to generate new frames.
+ */
+
+// Conway's game of life
+// Contributed by: Vliedel
+class GameLife : public ThreadedCanvasManipulator {
+public:
+  GameLife(Canvas *m, int delay_ms=500, bool torus=true)
+    : ThreadedCanvasManipulator(m), delay_ms_(delay_ms), torus_(torus) {
+    width_ = canvas()->width();
+    height_ = canvas()->height();
+
+    // Allocate memory
+    values_ = new int*[width_];
+    for (int x=0; x<width_; ++x) {
+      values_[x] = new int[height_];
+    }
+    newValues_ = new int*[width_];
+    for (int x=0; x<width_; ++x) {
+      newValues_[x] = new int[height_];
+    }
+
+    // Init values randomly
+    srand(time(NULL));
+    for (int x=0; x<width_; ++x) {
+      for (int y=0; y<height_; ++y) {
+        values_[x][y]=rand()%2;
+      }
+    }
+
+    r_ = rand()%255;
+    g_ = 255;
+    b_ = rand()%255;
+  }
+
+  ~GameLife() {
+    for (int x=0; x<width_; ++x) {
+      delete [] values_[x];
+    }
+    delete [] values_;
+    for (int x=0; x<width_; ++x) {
+      delete [] newValues_[x];
+    }
+    delete [] newValues_;
+  }
+
+  void Run() {
+    // True = down, up = false
+    bool rDirection = true;
+    bool bDirection = false;
+    int rStepDown = 5;
+    int rStepUp = 5;	   
+    int bStepDown = 5;
+    int bStepUp = 5;
+    int stepCounter = 0;
+
+    while (running() && !interrupt_received) {
+      if(stepCounter > 350){
+        // Randomise values and reset stepcounter
+        for (int x=0; x<width_; ++x) {
+      	  for (int y=0; y<height_; ++y) {
+            values_[x][y]=rand()%2;
+   	  }
+    	}
+        stepCounter = 0;
+      }
+      updateValues();
+
+      for (int x=0; x<width_; ++x) {
+        for (int y=0; y<height_; ++y) {
+          if (values_[x][y])
+            canvas()->SetPixel(x, y, r_, g_, b_);
+          else
+            canvas()->SetPixel(x, y, 0, 0, 0);
+        }
+      }
+	
+      // Red colour change
+      if (rDirection) {
+          if (r_ >= rStepDown) {
+            r_ -= rStepDown;
+          } else {
+            rDirection = !rDirection;
+          } 
+      } else {
+          if (r_ < 255 - rStepUp) {
+            r_ += rStepUp;
+          } else {
+            rDirection = !rDirection;
+          } 
+      }
+
+      // Blue colour change
+      if (bDirection) {
+          if (b_ >= bStepDown) {
+            b_ -= bStepDown;
+          } else {
+            bDirection = !bDirection;
+          } 
+      } else {
+          if (b_ < 255 - bStepUp) {
+            b_ += bStepUp;
+          } else {
+            bDirection = !bDirection;
+          } 
+      }
+
+      rStepDown = rand()%20;
+      rStepUp = rand()%20;	   
+      bStepDown = rand()%20;
+      bStepUp = rand()%20;
+
+      stepCounter += 1;
+
+      usleep(delay_ms_ * 3000); // ms
+    }
+    exit(0);
+  }
+
+private:
+  int numAliveNeighbours(int x, int y) {
+    int num=0;
+    if (torus_) {
+      // Edges are connected (torus)
+      num += values_[(x-1+width_)%width_][(y-1+height_)%height_];
+      num += values_[(x-1+width_)%width_][y                    ];
+      num += values_[(x-1+width_)%width_][(y+1        )%height_];
+      num += values_[(x+1       )%width_][(y-1+height_)%height_];
+      num += values_[(x+1       )%width_][y                    ];
+      num += values_[(x+1       )%width_][(y+1        )%height_];
+      num += values_[x                  ][(y-1+height_)%height_];
+      num += values_[x                  ][(y+1        )%height_];
+    }
+    else {
+      // Edges are not connected (no torus)
+      if (x>0) {
+        if (y>0)
+          num += values_[x-1][y-1];
+        if (y<height_-1)
+          num += values_[x-1][y+1];
+        num += values_[x-1][y];
+      }
+      if (x<width_-1) {
+        if (y>0)
+          num += values_[x+1][y-1];
+        if (y<31)
+          num += values_[x+1][y+1];
+        num += values_[x+1][y];
+      }
+      if (y>0)
+        num += values_[x][y-1];
+      if (y<height_-1)
+        num += values_[x][y+1];
+    }
+    return num;
+  }
+
+  void updateValues() {
+    // Copy values to newValues
+    for (int x=0; x<width_; ++x) {
+      for (int y=0; y<height_; ++y) {
+        newValues_[x][y] = values_[x][y];
+      }
+    }
+    // update newValues based on values
+    for (int x=0; x<width_; ++x) {
+      for (int y=0; y<height_; ++y) {
+        int num = numAliveNeighbours(x,y);
+        if (values_[x][y]) {
+          // cell is alive
+          if (num < 2 || num > 3)
+            newValues_[x][y] = 0;
+        }
+        else {
+          // cell is dead
+          if (num == 3)
+            newValues_[x][y] = 1;
+        }
+      }
+    }
+    // copy newValues to values
+    for (int x=0; x<width_; ++x) {
+      for (int y=0; y<height_; ++y) {
+        values_[x][y] = newValues_[x][y];
+      }
+    }
+  }
+
+  int** values_;
+  int** newValues_;
+  int delay_ms_;
+  int r_;
+  int g_;
+  int b_;
+  int width_;
+  int height_;
+  bool torus_;
+};
+
+static int usage(const char *progname) {
+  fprintf(stderr, "usage: %s <options> -D <demo-nr> [optional parameter]\n",
+          progname);
+  fprintf(stderr, "Options:\n");
+  fprintf(stderr,
+          "\t-D <demo-nr>              : Always needs to be set\n"
+          "\t-L                        : Large display, in which each chain is 'folded down'\n"
+          "\t                            in the middle in an U-arrangement to get more vertical space.\n"
+          "\t-R <rotation>             : Sets the rotation of matrix. "
+          "Allowed: 0, 90, 180, 270. Default: 0.\n"
+          "\t-t <seconds>              : Run for these number of seconds, then exit.\n");
+
+
+  rgb_matrix::PrintMatrixFlags(stderr);
+
+  fprintf(stderr, "Demos, choosen with -D\n");
+  fprintf(stderr, "\t0  - some rotating square\n"
+          "\t1  - forward scrolling an image (-m <scroll-ms>)\n"
+          "\t2  - backward scrolling an image (-m <scroll-ms>)\n"
+          "\t3  - test image: a square\n"
+          "\t4  - Pulsing color\n"
+          "\t5  - Grayscale Block\n"
+          "\t6  - Abelian sandpile model (-m <time-step-ms>)\n"
+          "\t7  - Conway's game of life (-m <time-step-ms>)\n"
+          "\t8  - Langton's ant (-m <time-step-ms>)\n"
+          "\t9  - Volume bars (-m <time-step-ms>)\n"
+          "\t10 - Evolution of color (-m <time-step-ms>)\n"
+          "\t11 - Brightness pulse generator\n");
+  fprintf(stderr, "Example:\n\t%s -t 10 -D 1 runtext.ppm\n"
+          "Scrolls the runtext for 10 seconds\n", progname);
+  return 1;
+}
+
+int main(int argc, char *argv[]) {
+  int runtime_seconds = -1;
+  int demo = -1;
+  int scroll_ms = 30;
+  int rotation = 0;
+  bool large_display = false;
+
+  const char *demo_parameter = NULL;
+  RGBMatrix::Options matrix_options;
+  rgb_matrix::RuntimeOptions runtime_opt;
+
+  // These are the defaults when no command-line flags are given.
+  matrix_options.rows = 32;
+  matrix_options.chain_length = 1;
+  matrix_options.parallel = 1;
+
+  // First things first: extract the command line flags that contain
+  // relevant matrix options.
+  if (!ParseOptionsFromFlags(&argc, &argv, &matrix_options, &runtime_opt)) {
+    return usage(argv[0]);
+  }
+
+  int opt;
+  while ((opt = getopt(argc, argv, "dD:t:r:P:c:p:b:m:LR:")) != -1) {
+    switch (opt) {
+    case 'D':
+      demo = atoi(optarg);
+      break;
+
+    case 't':
+      runtime_seconds = atoi(optarg);
+      break;
+
+    case 'm':
+      scroll_ms = atoi(optarg);
+      break;
+
+    case 'R':
+      rotation = atoi(optarg);
+      break;
+
+    case 'L':
+      if (matrix_options.chain_length == 1) {
+        // If this is still default, force the 64x64 arrangement.
+        matrix_options.chain_length = 4;
+      }
+      large_display = true;
+      break;
+
+      // These used to be options we understood, but deprecated now. Accept
+      // but don't mention in usage()
+    case 'd':
+      runtime_opt.daemon = 1;
+      break;
+
+    case 'r':
+      matrix_options.rows = atoi(optarg);
+      break;
+
+    case 'P':
+      matrix_options.parallel = atoi(optarg);
+      break;
+
+    case 'c':
+      matrix_options.chain_length = atoi(optarg);
+      break;
+
+    case 'p':
+      matrix_options.pwm_bits = atoi(optarg);
+      break;
+
+    case 'b':
+      matrix_options.brightness = atoi(optarg);
+      break;
+
+    default: /* '?' */
+      return usage(argv[0]);
+    }
+  }
+
+  if (optind < argc) {
+    demo_parameter = argv[optind];
+  }
+
+  if (demo < 0) {
+    fprintf(stderr, TERM_ERR "Expected required option -D <demo>\n" TERM_NORM);
+    return usage(argv[0]);
+  }
+
+  if (rotation % 90 != 0) {
+    fprintf(stderr, TERM_ERR "Rotation %d not allowed! "
+            "Only 0, 90, 180 and 270 are possible.\n" TERM_NORM, rotation);
+    return 1;
+  }
+
+  RGBMatrix *matrix = CreateMatrixFromOptions(matrix_options, runtime_opt);
+  if (matrix == NULL)
+    return 1;
+
+  if (large_display) {
+    // Mapping the coordinates of a 32x128 display mapped to a square of 64x64.
+    // Or any other U-arrangement.
+    matrix->ApplyStaticTransformer(UArrangementTransformer(
+                                     matrix_options.parallel));
+  }
+
+  if (rotation > 0) {
+    matrix->ApplyStaticTransformer(RotateTransformer(rotation));
+  }
+
+  printf("Size: %dx%d. Hardware gpio mapping: %s\n",
+         matrix->width(), matrix->height(), matrix_options.hardware_mapping);
+
+  Canvas *canvas = matrix;
+
+  // The ThreadedCanvasManipulator objects are filling
+  // the matrix continuously.
+  ThreadedCanvasManipulator *image_gen = NULL;
+  switch (demo) {
+
+  case 7:
+    image_gen = new GameLife(canvas, scroll_ms);
+    break;
+
+  }
+
+  if (image_gen == NULL)
+    return usage(argv[0]);
+
+  // Set up an interrupt handler to be able to stop animations while they go
+  // on. Note, each demo tests for while (running() && !interrupt_received) {},
+  // so they exit as soon as they get a signal.
+  signal(SIGTERM, InterruptHandler);
+  signal(SIGINT, InterruptHandler);
+
+  // Image generating demo is crated. Now start the thread.
+  image_gen->Start();
+
+  // Now, the image generation runs in the background. We can do arbitrary
+  // things here in parallel. In this demo, we're essentially just
+  // waiting for one of the conditions to exit.
+  if (runtime_seconds > 0) {
+    sleep(runtime_seconds);
+  } else {
+    // The
+    printf("Press <CTRL-C> to exit and reset LEDs\n");
+    while (!interrupt_received) {
+      sleep(1); // Time doesn't really matter. The syscall will be interrupted.
+    }
+  }
+
+  // Stop image generating thread. The delete triggers
+  delete image_gen;
+  delete canvas;
+
+  printf("\%s. Exiting.\n",
+         interrupt_received ? "Received CTRL-C" : "Timeout reached");
+  return 0;
+}
